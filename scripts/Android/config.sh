5a482bbb019fd5449422d92f74198ed112d54e56
Florine0928 <florine0928@gmail.com>
2024-10-19 00:18:41 +0300

    scripts: android: buildsystem: refine variables, add custom toolchain selection

--- scripts/Android/config.sh
+++ scripts/Android/config.sh
@@ -22,29 +22,59 @@
 # Kernel Name
 export IMAGE_NAME="E9K"
 
-# Exynos9810 is ARM64 or less-know as aarch64
+# Platform
+export PLATFORM_BUILD="Exynos9810"
+export PLATFORM_ARCH="exynos"
+
+###########################################################################################################################
+# Toolchains
+export TC_A="evagccc"
+export TC_AB="aarch64-elf-" # Toolchain GCC Binary
+export TC_AD="$TC_DIR/GCC/EVAGCC/"
+###########################################################################################################################
+export TC_B="gcc" # comes with every distro in existance..
+export TC_BB="aarch64-linux-gnu-" # Toolchain GCC Binary
+# feel free to add more Toolchains at will, considering your kernel supports it. However, Clang needs more work to get done.
+###########################################################################################################################
+
+# Toolchain Placeholder
+export TC="aarch64-linux-gnu-"
+
+# Toolchain Directory
+export TC_DIR="Toolchain"
+
+# Exynos9810 (ORRRR whatever the fuck is the SoC the end-user is building for..) is ARM64 or less-know as aarch64
 export ARCH=arm64
 export SUBARCH=arm64
 
 # aarch64 GCC Cross Compiler (Should be GCC 14 or later but U can try with GCC 4.9 too) 
-export CROSS_COMPILE=aarch64-linux-gnu-
+# Some compilers such as EvaGCC mentioned in Toolchain list have different GCC Binary name, for example EvaGCC is "aarch64-elf-"
+export CROSS_COMPILE=$TC
 
 # Some Samsung Shenanigans
 export ANDROID_MAJOR_VERSION=q
 
-# Defconfigs
-export star="exynos9810-starlte_defconfig"   # Samsung S9
-export starshort="starlte"
-export star2="exynos9810-star2lte_defconfig" # Samsung S9 Plus
-export star2short="star2lte"
-export crown="exynos9810-crownlte_defconfig" # Samsung Note9
-export crownshort="crownlte"
+# Defconfigs / Codenames / DTBs
+export DEV_A="exynos9810-starlte_defconfig"   # Samsung S9
+export DEV_AD="arch/$ARCH/boot/dts/$PLATFORM_ARCH/exynos9810-starlte_eur_open_26.dtb"
+export DEV_AC="starlte"
+
+export DEV_B="exynos9810-star2lte_defconfig" # Samsung S9 Plus
+export DEV_BD="arch/$ARCH/boot/dts/$PLATFORM_ARCH/exynos9810-star2lte_eur_open_26.dtb"
+export DEV_BC="star2lte"
+
+export DEV_C="exynos9810-crownlte_defconfig" # Samsung Note9
+export DEV_CD="arch/$ARCH/boot/dts/$PLATFORM_ARCH/exynos9810-crownlte_eur_open_26.dtb"
+export DEV_CC="crownlte"
+# format is in DEV_A, DEV_B... 
+# DEV_A plus a C means codename of the device
+# DEV_A plus a D means compiled dtb, so replace the dtb name with your device dts but end the file with .dtb instead of .dts or .dtsi
 
 # DTS Directory
-export DTSD=arch/$ARCH/boot/dts/exynos
+export DEV_DTS=arch/$ARCH/boot/dts/$PLATFORM_ARCH # exynos,qcom,mtk whatever you want..
 
 # Defaults (Device and Build Mode)
-export DEVICE=$star # Starlte Aka S9
+export DEVICE=$DEV_A # Starlte Aka S9
 export MODE="dirty" # Dirty Build
 
 # Main Directory
@@ -65,14 +95,14 @@ export RAMDISK=$DIR/E9K-Tools/Ramdisk
 # Compiled image name and location (Image/zImage)
 export KERNEL=$DIR/arch/$ARCH/boot/Image
 
-# Compiled DTB - Default Starlte
-export CDTB="arch/arm64/boot/dts/exynos/exynos9810-starlte_eur_open_26.dtb"
+# Compiled DTB - Default to DEVICE A
+export DEV_DTB="arch/$ARCH/boot/dts/$PLATFORM_ARCH/$DEV_AD"
 
 # defconfig dir
 export DEFCONFIG=$DIR/arch/$ARCH/configs
 
 # Unset some vars for Make Boot.img
-unset MAKEBOOT
+unset TARGET_MAKE_BOOTIMG
 
 ########################################################################
 #                                                                      #
--- scripts/Android/init.sh
+++ scripts/Android/init.sh
@@ -15,6 +15,18 @@
 # limitations under the License.
 #
 
+########################################################################
+# Function for Toolchain                                               # 
+########################################################################
+
+if [ ! -d "Toolchain" ]; then
+    echo "Toolchain directory is missing, we will create it"
+    mkdir Toolchain
+    mkdir Toolchain/GCC
+    mkdir Toolchain/TEMP
+    mkdir Toolchain/CLANG
+fi
+
 #############################################
 # Source the variables from the config file #
 source ./config.sh                          #
@@ -23,39 +35,44 @@ source ./config.sh                          #
 # ExremeXT lurks in this script
 usage() {
     echo "Usage: $0 [-d <platform>] [-m <clean/dirty>]"
-    echo "Example Usage: $0 -d S9 -m dirty -e"
+    echo "Example Usage: $0 -d $DEV_AC -t gcc -m dirty -e"
     echo "Options:"
-    echo "  -b          Build Kernel for S9,S9+,N9"
-    echo "  -m          Build Mode (clean/dirty"
+    echo "  -b          Build Kernel for $DEV_AC,$DEV_BC,$DEV_CC"
+    echo "  -m          Build Mode clean|dirty"
+    echo "  -t          Toolchain"
     echo "  -e          Make boot.img"
     echo "  -h          Display this help message"
+    echo "Available Toolchains:"
+    echo "   1: $TC_A"
+    echo "   2: $TC_B"
     exit 1
 }
 
 # Parse options
-while getopts "em:b:h" opt; do
+while getopts "em:b:t:h" opt; do
     case $opt in
         b)
             arg=$OPTARG  # Capture the argument for -b
 
-            if [[ "$arg" == "S9" ]]; then
+            if [[ "$arg" == "$DEV_AC" ]]; then
                 echo "Building kernel for: $arg"
-                export DEVICE=$star
-                export CDTB="arch/arm64/boot/dts/exynos/exynos9810-starlte_eur_open_26.dtb"
-            elif [[ "$arg" == "S9+" ]]; then
+                export DEVICE=$DEV_A
+                export DEV_DTB=$DEV_AD
+            elif [[ "$arg" == "$DEV_BC" ]]; then
                 echo "Building kernel for: $arg"
-                export DEVICE=$star2
-                export CDTB="arch/arm64/boot/dts/exynos/exynos9810-star2lte_eur_open_26.dtb"
-            elif [[ "$arg" == "N9" ]]; then
+                export DEVICE=$DEV_B
+                export DEV_DTB=$DEV_BD
+            elif [[ "$arg" == "$DEV_CC" ]]; then
                 echo "Building kernel for: $arg"
-                export DEVICE=$crown
-                export CDTB="arch/arm64/boot/dts/exynos/exynos9810-crownlte_eur_open_26.dtb"
+                export DEVICE=$DEV_C
+                export DEV_DTB=$DEV_CD
             else            
-                echo "Invalid argument for -b: Defaulting to S9"
+                echo "Invalid argument for -b: promptly exiting.."
+                exit
             fi
             ;;
         e)
-                export MAKEBOOT="true"
+                export TARGET_MAKE_BOOTIMG="true"
             ;;
         m)
             arg2=$OPTARG  # Capture the argument for -m
@@ -68,12 +85,25 @@ while getopts "em:b:h" opt; do
 
             else            
                 echo "invalid argument for -m: defaulting to dirty"
+                export MODE="dirty"
             fi
             ;;
         h)
             # Print Help Option (ex: ./init.sh -h)
             usage
             ;;
+        t)
+            arg3=$OPTARG  # Capture the argument for -t
+            if [[ "$arg3" == "$TC_A" ]]; then
+            export TC="$TC_AD/bin/$TC_AB"
+            elif [[ "$arg3" == "$TC_B" ]]; then
+            export TC=$TC_BB
+            else            
+            echo "WARNING: No toolchain selected, falling back to GNU GCC"
+            export TC=$TC_BB
+            fi
+
+        ;;
     esac
 done
 
@@ -105,14 +135,13 @@ make $DEVICE
 
 # Build Kernel
 build() {
-    if [[ "$MODE" == "dirty" ]]; then
     #If in dirty mode then make kernel while not cleaning workdir 
     make -j$(nproc)
-    else
-    # If not in dirty mode then clear workdir and make kernel
+    # If not in dirty mode a extra function with be invoked, refer to func/build_clean at line #116
+}
+
+build_clean() {
     make clean && make mrproper
-    make -j$(nproc)
-    fi
 }
 
 ########################################################################
@@ -129,7 +158,7 @@ PACK_BOOT_IMG()
 	cp -rf $RAMDISK/* $AIK
 	# Move Compiled kernel and dtb to A.I.K Folder
 	mv $KERNEL $AIK/split_img/boot.img-zImage
-	mv $CDTB $AIK/split_img/boot.img-dtb
+	mv $DEV_DTB $AIK/split_img/boot.img-dtb
 	# Create boot.img
 	$AIK/repackimg.sh
 	if [ ! -e $AIK/image-new.img ]; then
@@ -143,7 +172,7 @@ PACK_BOOT_IMG()
 	if [ ! -e $PRODUCT ]; then
         mkdir $PRODUCT
 	fi
-	cp $AIK/image-new.img $PRODUCT/$IMAGE_NAME.img
+	cp $AIK/image-new.img $PRODUCT/$IMAGE_NAME-$PLATFORM_BUILD.img
 	# Move boot.img to out dir
 	if [ ! -e $OUT ]; then
         mkdir $OUT
@@ -160,16 +189,24 @@ PACK_BOOT_IMG()
 # Initialize Kernel Building                                           #
 ########################################################################
 
+# IF mode is dirty, proceed, else, invoke clean build process.
+if [[ "$MODE" == "dirty" ]]; then
+    echo "WARNING: Kernel Build initialized in Dirty mode"
+else
+    build_clean
+fi
+
+
 # Make Defconfig
 defconfig
 
 # Make Kernel
 build
 
-if [[ "$MAKEBOOT" == "true" ]]; then
+if [[ "$TARGET_MAKE_BOOTIMG" == "true" ]]; then
 # Creates boot.img which will appear in E9K-Tools/Product folder
 PACK_BOOT_IMG
 else
-echo "boot.img won't be made"
+echo "WARNING: A BOOT.IMG WON'T BE CREATED"
 fi
 
